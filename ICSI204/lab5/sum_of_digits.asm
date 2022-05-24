6f9ba8cde5f2c257923d152879bcba74856d798a
enkhamar1220@gmail.com <enkhamar1220@gmail.com>
2022-05-24 09:49:44 +0800

    lab5: sum_of_digits

--- ICSI204/lab5/sum_of_digits.asm
+++ ICSI204/lab5/sum_of_digits.asm
@@ -2,5 +2,22 @@ ORG 100h
 
 mov ah, 1
 int 21h
+mov ah, 0
+
+cmp ah, 30     ; 0
+jmp LESS       ; BL < BH
+
+cmp ah, 39     ; 9
+jg GREATER     ; BL > BH
+
+
+
+LESS:
+GREATER:
+mov dx, offset msg
+mov ah, 9
+int 21h
+
+msg db "Please enter number"
 
 RET
