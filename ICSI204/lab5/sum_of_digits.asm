440f7f335d6559727dc21d3a4d553b7ee2958155
EnkhAmar <enkhamar1220@gmail.com>
2022-05-24 11:17:52 +0800

    lab5: almost working (Big numbers got wrong)

--- ICSI204/lab5/sum_of_digits.asm
+++ ICSI204/lab5/sum_of_digits.asm
@@ -2,22 +2,53 @@ ORG 100h
 
 mov ah, 1
 int 21h
+
+mov bl, al
+
 mov ah, 0
+mov cx, 0
 
-cmp ah, 30     ; 0
-jmp LESS       ; BL < BH
+mov dl, ' '
+mov ah, 2
+int 21h
 
-cmp ah, 39     ; 9
+cmp bl, 30h    ; 0
+jl LESS        ; BL < BH
+
+cmp bl, 39h    ; 9
 jg GREATER     ; BL > BH
 
+sub bl, 30h
+mov dx, 0
 
+LOOP1:
+cmp bx, si
+jl PRINT_RESULT
+add dl, array[si]
+inc si
+loop LOOP1
+
+jmp EXIT
 
 LESS:
 GREATER:
+
 mov dx, offset msg
 mov ah, 9
 int 21h
 
-msg db "Please enter number"
+jmp EXIT
+
+PRINT_RESULT:
+mov ax, dx
+aam
+add dl, 30h
+mov ah, 2
+int 21h
+
+EXIT:
 
 RET
+
+msg db "Please enter number$"
+array db 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
